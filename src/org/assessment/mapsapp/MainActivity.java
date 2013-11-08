c9264d710bf833c7c5835eb7c03845558e0e5b4e
Francisco Gomez <mail@frncscgmz.me>
2013-11-08 13:54:02 -0800

    Indent

--- src/org/assessment/mapsapp/MainActivity.java
+++ src/org/assessment/mapsapp/MainActivity.java
@@ -31,202 +31,202 @@ import android.util.Log;
 
 public class MainActivity extends Activity {
 
-	private static final GeoPoint BERLIN = new GeoPoint(52.51, 13.40);
-	private static final double SEARCH_RADIUS_KM = 2;
-	private static final int CONECTION_TIMEOUT   = 10000;
-        private static final String URL_CAR_SERVICE  = 
-           "https://www.drive-now.com/php/metropolis/json.vehicle_filter?cit=6099";
-        private final String DEBUG_TAG = this.getClass().
-           getSimpleName();
-	
-	private IMapController controller;
-	private MapView map;
-	
-	private static class Car {
-		
-		public GeoPoint position;
-		public String name;
-		public String address;
+   private static final GeoPoint BERLIN = new GeoPoint(52.51, 13.40);
+   private static final double SEARCH_RADIUS_KM = 2;
+   private static final int CONECTION_TIMEOUT   = 10000;
+   private static final String URL_CAR_SERVICE  = 
+      "https://www.drive-now.com/php/metropolis/json.vehicle_filter?cit=6099";
+   private final String DEBUG_TAG = this.getClass().
+      getSimpleName();
 
-		public Car(GeoPoint position, String name, String address) {
-			this.position = position;
-			this.name = name;
-			this.address = address;
-		}
-	}
+   private IMapController controller;
+   private MapView map;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		initLocation();
-	}
-	
-	private void initLocation() {
-		this.map = (MapView) this.findViewById(R.id.mapview);
-		this.map.setBuiltInZoomControls(true);
-		this.controller = map.getController();
+   private static class Car {
 
-		updateLocation(BERLIN);
-	}
-	
-        private void updateLocation(GeoPoint location){
-           this.controller.setZoom(12);
-           this.controller.setCenter(location);
+      public GeoPoint position;
+      public String name;
+      public String address;
 
-           List<Car> carsInCloseRadius = filterCarsByDistance(getCars(), location, SEARCH_RADIUS_KM);
+      public Car(GeoPoint position, String name, String address) {
+         this.position = position;
+         this.name = name;
+         this.address = address;
+      }
+   }
 
-           //  ###################################### TO IMPLEMENT  ###########################################
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      initLocation();
+   }
 
-           // Show cars on map
-           if(carsInCloseRadius.size() > 0) {
-              Log.d(DEBUG_TAG,"carsInCloseRadius: "+carsInCloseRadius.size());
-              showCarsOnMap(carsInCloseRadius);
-           }
-        }
+   private void initLocation() {
+      this.map = (MapView) this.findViewById(R.id.mapview);
+      this.map.setBuiltInZoomControls(true);
+      this.controller = map.getController();
 
-        private List<Car> getCars() {
+      updateLocation(BERLIN);
+   }
 
-           //  ###################################### TO IMPLEMENT  ###########################################
+   private void updateLocation(GeoPoint location){
+      this.controller.setZoom(12);
+      this.controller.setCenter(location);
 
-           // Read the JSON data from https://www.drive-now.com/php/metropolis/json.vehicle_filter?cit=6099
-           // Parse the data and fill a list of Car objects
-           List<Car> lstCars = new ArrayList<Car>();
-           try {
-              lstCars = new CarsAsyncTask().execute(URL_CAR_SERVICE).get();
-           } catch(Exception e) {}
+      List<Car> carsInCloseRadius = filterCarsByDistance(getCars(), location, SEARCH_RADIUS_KM);
 
-           return lstCars;
-        }
+      //  ###################################### TO IMPLEMENT  ###########################################
 
-        public void goClick(View view) {
-           // Take latitude and longitude
-           updateLocation(BERLIN);
-        }
+      // Show cars on map
+      if(carsInCloseRadius.size() > 0) {
+         Log.d(DEBUG_TAG,"carsInCloseRadius: "+carsInCloseRadius.size());
+         showCarsOnMap(carsInCloseRadius);
+      }
+   }
 
-        private class CarsAsyncTask extends AsyncTask<String, Void, List<Car>> {
+   private List<Car> getCars() {
 
-           @Override
-           protected List<Car> doInBackground(String... values) {
-              try {
-                 return loadCarsFromNetwork(values[0]);
-              } catch (IOException e) {
-                 Log.e(DEBUG_TAG,"Error: " + e);
-                 return new ArrayList<Car>();
-              } catch(JSONException e) {
-                 Log.e(DEBUG_TAG,"Error: " + e);
-                 return new ArrayList<Car>();
-              }
-           }
+      //  ###################################### TO IMPLEMENT  ###########################################
 
-           private List<Car> loadCarsFromNetwork(String urlString) 
-              throws IOException,JSONException {
-              String jsonResult = null;
-              List<Car> lstCars = null;
+      // Read the JSON data from https://www.drive-now.com/php/metropolis/json.vehicle_filter?cit=6099
+      // Parse the data and fill a list of Car objects
+      List<Car> lstCars = new ArrayList<Car>();
+      try {
+         lstCars = new CarsAsyncTask().execute(URL_CAR_SERVICE).get();
+      } catch(Exception e) {}
 
-              jsonResult = downloadUrl(urlString);
-              if(jsonResult != null) {
-                 Log.d(DEBUG_TAG,jsonResult);
-                 lstCars = parseJson(jsonResult);
-              }
+      return lstCars;
+   }
 
-              return lstCars;
-           }
+   public void goClick(View view) {
+      // Take latitude and longitude
+      updateLocation(BERLIN);
+   }
 
-           private String downloadUrl(String urlString) 
-              throws IOException {
-              String jsonResult = null;
-              String line       = null;
-              StringBuilder sb  = new StringBuilder(); 
+   private class CarsAsyncTask extends AsyncTask<String, Void, List<Car>> {
 
-              URL serverAddress             = new URL(urlString);
-              HttpURLConnection connection  = (HttpURLConnection)serverAddress
-                 .openConnection();
-              connection.setConnectTimeout(CONECTION_TIMEOUT);
-              connection.connect();
+      @Override
+      protected List<Car> doInBackground(String... values) {
+         try {
+            return loadCarsFromNetwork(values[0]);
+         } catch (IOException e) {
+            Log.e(DEBUG_TAG,"Error: " + e);
+            return new ArrayList<Car>();
+         } catch(JSONException e) {
+            Log.e(DEBUG_TAG,"Error: " + e);
+            return new ArrayList<Car>();
+         }
+      }
 
-              BufferedReader br = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()));
-              while((line = br.readLine()) != null) {
-                 sb.append(line);
-              }
-              br.close();
-              jsonResult = sb.toString();
+      private List<Car> loadCarsFromNetwork(String urlString) 
+         throws IOException,JSONException {
+         String jsonResult = null;
+         List<Car> lstCars = null;
 
-              return jsonResult;
-           }
+         jsonResult = downloadUrl(urlString);
+         if(jsonResult != null) {
+            Log.d(DEBUG_TAG,jsonResult);
+            lstCars = parseJson(jsonResult);
+         }
 
-           private List<Car> parseJson(String jsonResult) throws JSONException {
-              List<Car> lstCars = new ArrayList<Car>();
+         return lstCars;
+      }
 
-              JSONObject root = new JSONObject(jsonResult);
-              if(root.has("rec")) {
-                 JSONObject recObj = root.getJSONObject("rec");
-                 JSONObject vehiclesObj = recObj.getJSONObject("vehicles");
-                 JSONArray vehiclesArray = vehiclesObj.getJSONArray("vehicles");
-                 for(int i = 0;i < vehiclesArray.length();i++) {
-                    JSONObject vehicleObj = vehiclesArray.getJSONObject(i);
-                    JSONObject positionObj = vehicleObj.getJSONObject("position");
+      private String downloadUrl(String urlString) 
+         throws IOException {
+         String jsonResult = null;
+         String line       = null;
+         StringBuilder sb  = new StringBuilder(); 
 
-                    double latitude = positionObj.getDouble("latitude");
-                    double longitude = positionObj.getDouble("longitude");
-                    GeoPoint coords = new GeoPoint(latitude, longitude);
+         URL serverAddress             = new URL(urlString);
+         HttpURLConnection connection  = (HttpURLConnection)serverAddress
+            .openConnection();
+         connection.setConnectTimeout(CONECTION_TIMEOUT);
+         connection.connect();
 
-                    lstCars.add(new Car(coords,vehicleObj.getString("carName"),
-                          positionObj.getString("address")));
-                 }
-              }
-              return lstCars;
-           }
-        }
+         BufferedReader br = new BufferedReader(
+               new InputStreamReader(connection.getInputStream()));
+         while((line = br.readLine()) != null) {
+            sb.append(line);
+         }
+         br.close();
+         jsonResult = sb.toString();
 
-	
-        private List<Car> filterCarsByDistance(List<Car> cars, GeoPoint location, double searchRadiusKm) {
+         return jsonResult;
+      }
 
-           //  ###################################### TO IMPLEMENT  ###########################################
+      private List<Car> parseJson(String jsonResult) throws JSONException {
+         List<Car> lstCars = new ArrayList<Car>();
 
-           // Filter the list of cars by a location within the specified radius
+         JSONObject root = new JSONObject(jsonResult);
+         if(root.has("rec")) {
+            JSONObject recObj = root.getJSONObject("rec");
+            JSONObject vehiclesObj = recObj.getJSONObject("vehicles");
+            JSONArray vehiclesArray = vehiclesObj.getJSONArray("vehicles");
+            for(int i = 0;i < vehiclesArray.length();i++) {
+               JSONObject vehicleObj = vehiclesArray.getJSONObject(i);
+               JSONObject positionObj = vehicleObj.getJSONObject("position");
 
-           Log.d(DEBUG_TAG,"filterCarsByDistance: "+cars.size());
+               double latitude = positionObj.getDouble("latitude");
+               double longitude = positionObj.getDouble("longitude");
+               GeoPoint coords = new GeoPoint(latitude, longitude);
 
-           return cars;
-        }
-
-        private void showCarsOnMap(List<Car> cars) {
-           List<OverlayItem> items = new ArrayList<OverlayItem>();
-
-           for (Car car : cars){
-              OverlayItem olItem = new OverlayItem(car.name, car.address, car.position); 
-              items.add(olItem);
-           }
-
-           OnItemGestureListener<OverlayItem> listener = new OnItemGestureListener<OverlayItem>() {
-              @Override
-              public boolean onItemLongPress(int arg0, OverlayItem item) {
-                 Toast toast = Toast.makeText(getApplicationContext(), item.getTitle() + ", " + item.getSnippet(), Toast.LENGTH_LONG);
-                 toast.show();
-                 return false;
-              }
-              @Override
-              public boolean onItemSingleTapUp(int arg0, OverlayItem item) {
-                 Toast toast = Toast.makeText(getApplicationContext(), item.getTitle() + ", " + item.getSnippet(), Toast.LENGTH_LONG);
-                 toast.show();
-                 return false;
-              }};
-           ItemizedIconOverlay<OverlayItem> overlay = new ItemizedIconOverlay<OverlayItem>(getApplicationContext(), items, listener);
+               lstCars.add(new Car(coords,vehicleObj.getString("carName"),
+                        positionObj.getString("address")));
+            }
+         }
+         return lstCars;
+      }
+   }
 
 
-           this.map.getOverlays().clear();
-           this.map.getOverlays().add(overlay);
-           this.map.invalidate();
-        }
+   private List<Car> filterCarsByDistance(List<Car> cars, GeoPoint location, double searchRadiusKm) {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		
-		return true;
-	}
+      //  ###################################### TO IMPLEMENT  ###########################################
+
+      // Filter the list of cars by a location within the specified radius
+
+      Log.d(DEBUG_TAG,"filterCarsByDistance: "+cars.size());
+
+      return cars;
+   }
+
+   private void showCarsOnMap(List<Car> cars) {
+      List<OverlayItem> items = new ArrayList<OverlayItem>();
+
+      for (Car car : cars){
+         OverlayItem olItem = new OverlayItem(car.name, car.address, car.position); 
+         items.add(olItem);
+      }
+
+      OnItemGestureListener<OverlayItem> listener = new OnItemGestureListener<OverlayItem>() {
+         @Override
+         public boolean onItemLongPress(int arg0, OverlayItem item) {
+            Toast toast = Toast.makeText(getApplicationContext(), item.getTitle() + ", " + item.getSnippet(), Toast.LENGTH_LONG);
+            toast.show();
+            return false;
+         }
+         @Override
+         public boolean onItemSingleTapUp(int arg0, OverlayItem item) {
+            Toast toast = Toast.makeText(getApplicationContext(), item.getTitle() + ", " + item.getSnippet(), Toast.LENGTH_LONG);
+            toast.show();
+            return false;
+         }};
+      ItemizedIconOverlay<OverlayItem> overlay = new ItemizedIconOverlay<OverlayItem>(getApplicationContext(), items, listener);
+
+
+      this.map.getOverlays().clear();
+      this.map.getOverlays().add(overlay);
+      this.map.invalidate();
+   }
+
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.main, menu);
+
+      return true;
+   }
 
 }
